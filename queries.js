a35e1fb02ad8ad460513347bc06e41d4f9c3273f
Jordan Morales <jordan.morales@gmail.com>
2019-12-20 13:46:14 -0800

    modifying api stuff for deployment

--- queries.js
+++ queries.js
@@ -11,10 +11,9 @@ let connection = {
   port: 5432,
   database: 'd27p8cg8c6pl72',
   user: 'fmmhfdwbhagacr',
-  password: process.env.DB_PASSWORD,
+  password: '2169c3c5c74891eca065ae23b1328d16c029078557aeb315cbf78e93285d5430',
   ssl: true
 }
-
 
 
 let db = pgp(connection);
